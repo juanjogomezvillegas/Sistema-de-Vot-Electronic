82b1b62d52c206731bafbd1b33123ff299ddd5ca
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-27 12:42:08 +0100

    instalacio de vue.js

--- src/view/util/script.php
+++ src/view/util/script.php
@@ -1,6 +1,7 @@
 <!-- JQUERY I JQUERY UI -->
 <script src="js/jquery.js"></script>
 <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+<script src="js/vue.js"></script>
 
 <!-- Fitxers Java Script -->
 <script src="js/menu.js"></script>
