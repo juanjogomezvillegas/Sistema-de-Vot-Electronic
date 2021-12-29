40004a43c3989cd905f5bf95bb4271d81dba97e7
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-29 11:03:15 +0100

    canvis menors

--- src/view/admin.php
+++ src/view/admin.php
@@ -28,13 +28,13 @@
         <br>
         <div class="is-full mt-6">
             <div class="columns is-variable">
-                <div id="itemAdminVots" class="itemAdmin column m-4 is-flex is-justify-content-center is-align-items-center">
+                <div id="itemAdminVots" class="itemAdmin is-clickable column m-4 is-flex is-justify-content-center is-align-items-center">
                     <p class="mr-5 is-variable"><span class="icon"><i class="fas fa-poll is-size-1"></i></span></p>
                     <p class="mr-2 is-size-2 is-variable">Vots</p>
                     <p id="itemAdminVotsNum" class="is-size-2">0</p>
                 </div>
                 <?php if ($dadesUsuarilogat["rol"] === "Administrator") { ?>
-                    <div id="itemAdminUsuari" class="itemAdmin column m-4 is-flex is-justify-content-center is-align-items-center">
+                    <div id="itemAdminUsuari" class="itemAdmin is-clickable column m-4 is-flex is-justify-content-center is-align-items-center">
                         <p class="mr-5 is-variable"><span class="icon"><i class="fas fa-users is-size-1"></i></span></p>
                         <p class="mr-2 is-size-2 is-variable">Usuaris</p>
                         <p id="itemAdminUsuariNum" class="is-size-2">0</p>
@@ -47,7 +47,7 @@
                     </div>
                 <?php } ?>
                 <?php if ($dadesUsuarilogat["rol"] === "Administrator" || $dadesUsuarilogat["rol"] === "Manager") { ?>
-                    <div id="itemAdminCandidat" class="itemAdmin column m-4 is-flex is-justify-content-center is-align-items-center">
+                    <div id="itemAdminCandidat" class="itemAdmin is-clickable column m-4 is-flex is-justify-content-center is-align-items-center">
                         <p class="mr-5 is-variable"><span class="icon"><i class="fas fa-person-booth is-size-1"></i></span></p>
                         <p class="mr-2 is-size-2 is-variable">Candidats</p>
                         <p id="itemAdminCandidatNum" class="is-size-2">0</p>
