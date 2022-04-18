674680aada0c56ca4667444f42d8a05fd72009b9
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-04-18 09:44:41 +0200

    project laravel

---
+++ database/migrations/2022_04_18_073156_configurations.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configurations', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->default('Electronic Voting System');
+            $table->string('logo')->default('img/apli/logo-example.png');
+            $table->string('icon')->default('img/apli/icon-example.png');
+            $table->integer('seats')->default(100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configurations');
+    }
+};
---
+++ database/migrations/2022_04_18_073927_legislatures.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('legislatures', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->date('election');
+            $table->date('begin');
+            $table->date('end')->nullable(true);
+            $table->string('president')->default('');
+            $table->string('party')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('legislatures');
+    }
+};
