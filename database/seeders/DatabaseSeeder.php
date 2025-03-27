d4b4ed7704b7f194a6bd2a1a6f426c13c42700c9
juanjogomezvillegas <juanjogomvil@gmail.com>
2025-03-27 01:02:34 +0100

    solved problems

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
 
         $this->call([
             ConfigSeeder::class,
             UserSeeder::class,
-            CountrySeeder::class,
+            CountrySeeder::class
         ]);
     }
 }
